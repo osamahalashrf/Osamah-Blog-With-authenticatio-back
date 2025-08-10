b1eab0456e2d06a8b65f008e58f1e19a3d765bf4
osamahalashrf <osamahalashrf@gmail.com>
2025-08-10 09:49:12 +0300

    update Domain-4

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -2,4 +2,4 @@
 
 export const ARTICLE_PER_PAGE = 6; // عدد المقالات في كل صفحة
 //export const DOMAIN = "http://localhost:3000";
-export const DOMAIN = "https://osamah-blog-with-authenticatio.vercel.app/en"; // API endpoint for articles
+export const DOMAIN = "https://osamah-blog-with-authenticatio.vercel.app"; // API endpoint for articles
